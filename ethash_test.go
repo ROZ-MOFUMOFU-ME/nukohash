ef1306830d441224385a9279e4af0a92bed7e789
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2019-02-17 07:09:52 +0900

    Update ethash_test.go

--- ethash_test.go
+++ ethash_test.go
@@ -27,8 +27,8 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/nekonium/go-nekonium/common"
+	"github.com/nekonium/go-nekonium/crypto"
 )
 
 func init() {
